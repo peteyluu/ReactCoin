5e3562d883f5e6d0c5fc7eb454a250304a41b971
Peter Luu <peter.luu510@gmail.com>
2018-04-15 14:43:46 -0700

    Added Page component

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pagination = () => {
+  return (
+    <div>
+      <button>&larr;</button>
+      <button>&rarr;</button>
+    </div>
+  )
+};
+
+export default Pagination;
