626688afd6a37adc77466e40b84a0cccf716b5b6
Peter Luu <peter.luu510@gmail.com>
2018-04-15 12:49:46 -0700

    Added List component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/list/list';
 import './index.css';
 
 const App = () => {
-  const title = 'React Coin';
   return (
     <div>
       <Header />
-      <h1>{title}</h1>
+      <List />
     </div>
   );
 };
