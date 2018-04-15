689caf80eaf5f6a50d51a47272b344b378e4e97e
Peter Luu <peter.luu510@gmail.com>
2018-04-15 14:55:32 -0700

    Received props from List component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import page from '../css/page.css';
 
-const Pagination = () => {
+const Page = ({ page, totalPages }) => {
   return (
-    <div>
-      <button>&larr;</button>
-      <button>&rarr;</button>
+    <div className="page">
+      <button className="page-button">&larr;</button>
+      <span className="page-info">
+        page <b>{page}</b> of <b>{totalPages}</b>
+      </span>
+      <button className="page-button">&rarr;</button>
     </div>
   )
 };
 
-export default Pagination;
+export default Page;
