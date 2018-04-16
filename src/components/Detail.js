052ec8bd12eff63261f8d076e8bacb9f10309398
Peter Luu <peter.luu510@gmail.com>
2018-04-16 14:31:26 -0700

    Connected Detail to store

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,14 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchCurrency } from '../actions/currency';
 import '../css/detail.css';
 
 class Detail extends PureComponent {
+  componentDidMount() {
+    this.props.fetchCurrency(this.props.match.params.id);
+  }
+
   render () {
     return (
       <div>Detail</div>
@@ -9,4 +16,16 @@ class Detail extends PureComponent {
   }
 }
 
-export default Detail;
+Detail.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  currency: PropTypes.object.isRequired,
+  error: PropTypes.object,
+  fetchCurrency: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.currency;
+
+export default connect(
+  mapStateToProps,
+  { fetchCurrency },
+)(Detail);
