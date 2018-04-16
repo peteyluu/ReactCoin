68571f89c34f642a6c53d2f609202edda9597edd
Peter Luu <peter.luu510@gmail.com>
2018-04-15 22:00:49 -0700

    Added initial routers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import store from './store';
 import Header from './components/Header';
 import List from './components/List';
+import NotFound from './components/NotFound';
+import Detail from './components/Detail';
 import './css/index.css';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <Header />
-        <List />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={List} />
+            <Route exact path="/currency/:id" component={Detail} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 };
