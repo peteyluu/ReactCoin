48d5009310cc62434b3eb40b87177fcda4d0e80d
Peter Luu <peter.luu510@gmail.com>
2018-04-16 14:33:29 -0700

    Added currency reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import currenciesReducer from './currenciesReducer' ;
+import currenciesReducer from './currenciesReducer';
+import currencyReducer from './currencyReducer';
 
 export default combineReducers({
   currencies: currenciesReducer,
+  currency: currencyReducer,
 });
