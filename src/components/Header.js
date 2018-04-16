011ee5ebaa5fb997f2e5c8ccd9b941c1131c951b
Peter Luu <peter.luu510@gmail.com>
2018-04-16 15:21:02 -0700

    Added Link route to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import '../css/header.css';
 
 const Header = () => {
   return (
     <div className="header">
-      <img src={logo} className="header-logo" alt="logo" />
-      <span className="header-logo-text">Coin</span>
+      <Link to="/">
+        <img src={logo} className="header-logo" alt="logo" />
+        <span className="header-logo-text">Coin</span>
+      </Link>
     </div>
   );
 };
