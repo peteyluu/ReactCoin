af23c4f6bd16d80a23a6af3cd382cf1100964788
Peter Luu <peter.luu510@gmail.com>
2018-04-15 21:57:53 -0700

    Added Detail component

---
+++ src/components/Detail.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import '../css/detail.css';
+
+class Detail extends PureComponent {
+  render () {
+    return (
+      <div>Detail</div>
+    );
+  }
+}
+
+export default Detail;
