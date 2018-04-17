6d564dd10666123e17abf8a4f14b3a4541acd841
Peter Luu <peter.luu510@gmail.com>
2018-04-16 17:21:38 -0700

    Removed Link dependency

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchCurrency } from '../actions/currency';
 import Loading from './Loading';
